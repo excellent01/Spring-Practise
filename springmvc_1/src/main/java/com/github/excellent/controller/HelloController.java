e66d2a72ec49346708b5126f6ca06b9c6b068dd0
excellent01 <2418512129@qq.com>
2019-08-09 18:00:28 +0800

    IDEA + maven 搭建一个SpringMVC项目

---
+++ springmvc_1/src/main/java/com/github/excellent/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.github.excellent.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @auther plg
+ * @date 2019/8/9 17:51
+ */
+@Controller
+@RequestMapping(value = "/")
+public class HelloController {
+    @RequestMapping(path = "hello")
+    public String sayHello(){
+        System.out.println("hello Spring");
+        return "hello";
+    }
+}
